14640621047f1ed935b644eb4fe685b2e2a062f4
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-04 08:38:53 +0000

    add foor loop to interiate through itemslist

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def get_todo_list(repeat):
-    return render(request, 'todo/todo_list.html')
+    items = Items.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
 
